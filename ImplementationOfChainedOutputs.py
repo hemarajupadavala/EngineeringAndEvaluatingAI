a90a1139759f216ba40dbc7b3dde6d73b11acbfa
Hemaraju <hemarajupadavala@gmail.com>
2024-07-10 14:02:46 +0530

    Reading CSVs
    Reading the CSV's

--- ImplementationOfChainedOutputs.py
+++ ImplementationOfChainedOutputs.py
@@ -2,3 +2,9 @@ import pandas as pd
 import numpy as np
 read= pd.read_csv('AppGallery.csv')
 read.info()
+print("---------------------")
+import pandas as pd
+import numpy as np
+read= pd.read_csv("Purchasing.csv")
+read.info()
+print("___________________")
