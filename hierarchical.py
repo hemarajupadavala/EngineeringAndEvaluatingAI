16826472c4c85b692aa1c21507933f01f58ed081
Hemaraju <hemarajupadavala@gmail.com>
2024-07-10 14:21:33 +0530

    Text Processing

--- hierarchical.py
+++ hierarchical.py
@@ -16,3 +16,26 @@ from textblob import TextBlob
 nltk.download('punkt', quiet=True)
 nltk.download('stopwords', quiet=True)
 nltk.download('wordnet', quiet=True)
+class TextPreprocessor(BaseEstimator, TransformerMixin):
+    def __init__(self):
+        self.lemmatizer = WordNetLemmatizer()
+        self.stop_words = set(stopwords.words('english'))
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        return [self.preprocess(text) for text in X]
+
+    def preprocess(self, text):
+        tokens = nltk.word_tokenize(str(text).lower())
+        tokens = [self.lemmatizer.lemmatize(token) for token in tokens if token not in self.stop_words]
+        return ' '.join(tokens)
+
+class HierarchicalMultiOutputClassifier:
+    def __init__(self):
+        self.vectorizer = TfidfVectorizer(max_features=10000, ngram_range=(1, 3))
+        self.label_encoders = {}
+        self.models = {'Type 2': None, 'Type 3': {}, 'Type 4': {}}
+        self.text_preprocessor = TextPreprocessor()
+        
